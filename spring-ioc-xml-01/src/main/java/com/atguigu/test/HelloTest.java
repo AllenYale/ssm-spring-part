34e23f9e8d23ee2becae1b646c7b919f58188922
allen_yale_mac <137728688@qq.com>
2023-08-29 21:37:39 +0800

    Initial commit

---
+++ spring-ioc-xml-01/src/main/java/com/atguigu/test/HelloTest.java
@@ -0,0 +1,7 @@
+package com.atguigu.test;
+
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("hello world,hello java, this is final choice for hanyu's career");
+    }
+}
---
+++ spring-ioc-xml-01/src/test/java/com/atguigu/test/SpringIOCTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.test;
+
+import org.junit.jupiter.api.Test;
+
+public class SpringIOCTest {
+    @Test
+    public void test01(){
+        System.out.println("SpringIOCTest.test01");
+    }
+}
