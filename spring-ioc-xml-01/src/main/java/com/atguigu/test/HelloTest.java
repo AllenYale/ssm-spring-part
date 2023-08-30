085937c0f8cc72ef081b418572650e7b59c28ce0
hanyu_LEGION <137728688@qq.com>
2023-08-30 23:34:47 +0800

    Legion test git push...

--- spring-ioc-xml-01/src/main/java/com/atguigu/test/HelloTest.java
+++ spring-ioc-xml-01/src/main/java/com/atguigu/test/HelloTest.java
@@ -3,5 +3,6 @@ package com.atguigu.test;
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello world,hello java, this is final choice for hanyu's career");
+        System.out.println("hello test lenovo legion test...");
     }
 }
